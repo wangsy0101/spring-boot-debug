69266abbd22099b6b7c828fe7fb67672ac34045c
王圣约 <wangsy@wangshengyuedeMacBook-Pro.local>
2021-11-08 10:42:50 +0800

    hystrix

--- spring-boot-mysql/src/main/java/cn/wangsy/Application.java
+++ spring-boot-mysql/src/main/java/cn/wangsy/Application.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * Created by wangsy on 2021/7/27
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 @MapperScan(basePackages="cn.wangsy.dao")
+@EnableHystrix
 public class Application {
 
     public static void main(String[] args) {
--- spring-boot-mysql/src/main/java/cn/wangsy/service/AccountService.java
+++ spring-boot-mysql/src/main/java/cn/wangsy/service/AccountService.java
@@ -2,6 +2,7 @@ package cn.wangsy.service;
 
 import cn.wangsy.dao.AccountMapper;
 import cn.wangsy.entity.Account;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,18 +17,27 @@ public class AccountService {
     @Autowired
     private AccountMapper accountMapper;
 
-
+    @HystrixCommand(fallbackMethod = "hello")
     public Account queryOne(Long id){
-        Account account = accountMapper.queryOne(id);
-        // convert pojo
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e){
+
+        }
+        return accountMapper.queryOne(id);
+    }
+
+
+    public Account hello(Long id){
+        Account account = new Account();
+        account.setId(-1L);
         return account;
     }
+
 
     public List<Account> queryByName(String name){
         List<Account> accountList = accountMapper.queryByName(name);
         // convert pojo
         return accountList;
     }
-
-
 }
